1875f6eac02e90af6186361167830b7078a67ea3
Brunogfv <bruno.gfv@gmail.com>
2025-07-01 16:26:57 -0300

    Adição da classe Animal-inicio

--- src/miaudota/Animal.java
+++ src/miaudota/Animal.java
@@ -1,5 +1,33 @@
 package miaudota;
 
-public abstract class Animal {
+import java.util.Date;
 
+public abstract class Animal {
+    protected int id;
+    protected String nome;
+    protected String raca;
+    protected int idade;
+    protected String sexo;
+    protected String porte;
+    protected double peso;
+    protected String cor;
+    protected String descricao;
+    protected Date dataCadastro;
+    protected boolean vacinado;
+    protected boolean castrado;
+
+    public Animal(int id, String nome, String raca, int idade, String sexo, String porte, double peso, String cor, String descricao, Date dataCadastro, boolean vacinado, boolean castrado) {
+        this.id = id;
+        this.nome = nome;
+        this.raca = raca;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.porte = porte;
+        this.peso = peso;
+        this.cor = cor;
+        this.descricao = descricao;
+        this.dataCadastro = dataCadastro;
+        this.vacinado = vacinado;
+        this.castrado = castrado;
+    }
 }
